9192ec8c8f9b30f0fabb31d2d3170ec86f1e6b59
Sergey Artemov <sergeyartemov@yandex.ru>
2021-07-22 13:18:36 +0300

    CancelAsync

--- Program.cs
+++ Program.cs
@@ -6,12 +6,17 @@ namespace SampleAsyncAwait
 {
     class Program
     {
-        static void Factorial(int n = 6)
+        static void Factorial(int n = 6, CancellationToken token)
         {
             if (n < 1) throw new Exception($"{n}: число не должно быть меньше 1");
             int result = 1;
             for(int i=1; i<=n; i++)
             {
+                if (token.IsCancellationRequested)   // На каждой итерации проверяем - не прислали-ли извне Отмену Task(-а)
+                {
+                    Console.WriteLine("Операция прервана");
+                    return;
+                }
                 result *= i;
             }
 
@@ -20,13 +25,15 @@ namespace SampleAsyncAwait
         }
 
         //Определение асинхронного метода async-await
-        static async void FactorialAsync(int n)
+        static async void FactorialAsync(int n, CancellationToken token)
         {
             Console.WriteLine("Начало метода FactorialAsync"); // выполняется синхронно
 
+            if (token.IsCancellationRequested) return;
+
             try
             {
-                Task task = Task.Run(() => Factorial(n));// выполняется асинхронно
+                Task task = Task.Run(() => Factorial(n, token));// выполняется асинхронно
                 await task;
             }
             catch (Exception ex)
@@ -36,23 +43,23 @@ namespace SampleAsyncAwait
             Console.WriteLine("Конец метода FactorialAsync");
         }
 
-        static async void FactorialAsyncInSeries()
+        static async void FactorialAsyncInSeries(CancellationToken token)
         {
             //Выполняется последовательно
-            await Task.Run(() => Factorial(4));
-            await Task.Run(() => Factorial(3));
-            await Task.Run(() => Factorial(5));
+            await Task.Run(() => Factorial(4, token));
+            await Task.Run(() => Factorial(3, token));
+            await Task.Run(() => Factorial(5, token));
         }
 
-        static async void FactorialAsyncInParalell()
+        static async void FactorialAsyncInParalell(CancellationToken token)
         {
             Task alltasks = null;
             try
             {
                 //Выполняется паралелльно
-                Task t1 = Task.Run(() => Factorial(4));
-                Task t2 = Task.Run(() => Factorial(3));
-                Task t3 = Task.Run(() => Factorial(5));
+                Task t1 = Task.Run(() => Factorial(4, token));
+                Task t2 = Task.Run(() => Factorial(3, token));
+                Task t3 = Task.Run(() => Factorial(5, token));
                 //await Task.WhenAll(new[] { t1, t2, t3 });
                 alltasks = Task.WhenAll(new Task[] { t1, t2, t3 });
                 await alltasks;
@@ -71,12 +78,16 @@ namespace SampleAsyncAwait
 
         static void Main(string[] args)
         {
-            int n;            
-            
+            int n;
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+            CancellationToken token = tokenSource.Token;
+
             Console.WriteLine("Введите число:");
             n  = Int32.Parse(Console.ReadLine());
             
-            FactorialAsync(n);  // Вызов асинхронного метода
+            FactorialAsync(n, token);  // Вызов асинхронного метода
+
+            tokenSource.Cancel(); // Прерываем(отменяем) Task
 
 
             Console.WriteLine($"Квадрат числа равен {n*n}");
